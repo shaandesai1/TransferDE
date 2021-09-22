ec4502170e9568cdb0156e5adaaf10f825d96950
Shaan Desai <shaandesai@live.com>
2021-09-22 08:38:51 -0400

    system second order bundle

--- base_solver_ffnn_bundles_mathieu.py
+++ base_solver_ffnn_bundles_mathieu.py
@@ -18,16 +18,16 @@ import random
 parser = argparse.ArgumentParser('transfer demo')
 
 parser.add_argument('--tmax', type=float, default=3.14159)
-parser.add_argument('--dt', type=int, default=0.05)
+parser.add_argument('--dt', type=int, default=0.1)
 parser.add_argument('--niters', type=int, default=10000)
 parser.add_argument('--niters_test', type=int, default=15000)
 parser.add_argument('--hidden_size', type=int, default=100)
-parser.add_argument('--num_bundles', type=int, default=100)
+parser.add_argument('--num_bundles', type=int, default=10)
 parser.add_argument('--num_bundles_test', type=int, default=1000)
 parser.add_argument('--test_freq', type=int, default=100)
 parser.add_argument('--viz', action='store_false')
 parser.add_argument('--gpu', type=int, default=0)
-parser.add_argument('--evaluate_only', action='store_true')
+parser.add_argument('--evaluate_only', action='store_false')
 args = parser.parse_args()
 scaler = MinMaxScaler()
 
@@ -69,20 +69,6 @@ def get_udot(t,y,yd,a1,a0,f):
 
     ydd = (-a1s*yd -a0s * y + f0s)
     return ydd
-
-class SiLU(nn.Module):
-    def __init__(self):
-        '''
-        Init method.
-        '''
-        super().__init__() # init the base class
-
-    def forward(self, input):
-        '''
-        Forward pass of the function.
-        '''
-        return torch.sin(input)
-
 
 class base_diffeq:
     """
@@ -190,7 +176,7 @@ class Transformer_Learned(nn.Module):
 
 
 # h, hd, hdd, true_y0, t.detach(), a1_train, a0_train, f_train)
-def get_wout(s, sd,sdd, y0s, t,a1s,a0s,fs):
+def get_wout(s, sd,sdd, y0, t,a1s,a0s,fs):
     # y0 = torch.stack([y0 for _ in range(len(s))]).reshape(len(s), -1)
 
     a0_batch = torch.cat([var_(t) for var_ in a0s], 1)
@@ -198,7 +184,7 @@ def get_wout(s, sd,sdd, y0s, t,a1s,a0s,fs):
     f_batch = torch.cat([var_(t) for var_ in fs], 1)
     WS = []
     for i in range(f_batch.shape[1]):
-        y0 = y0s[i,:].reshape(1,-1)
+
         a0 = a0_batch[:,i].reshape(-1,1)
         a1 = a1_batch[:,i].reshape(-1,1)
         f = f_batch[:,i].reshape(-1,1)
@@ -208,7 +194,7 @@ def get_wout(s, sd,sdd, y0s, t,a1s,a0s,fs):
         D0 = -f
 
         h0m = s[0].reshape(-1, 1)
-        h0d = sd[0].reshape(-1, 1)
+        h0d = s[-1].reshape(-1, 1)
         W0 = torch.linalg.solve(DH.t() @ DH + h0m @ h0m.t() + h0d @ h0d.t(),
                                 -DH.t() @ D0 + h0m @ (y0[:,0].reshape(1, -1)) + h0d @ (y0[:,1].reshape(1, -1)))
         # print(W0.shape)
@@ -228,20 +214,20 @@ if args.viz:
     plt.show(block=False)
 
 
-def visualize(true_y, pred_y,pred_ydot, lst,t):
+def visualize(true_y, pred_y, lst,t):
     if args.viz:
         ax_traj.cla()
         ax_traj.set_title('Trajectories')
         ax_traj.set_xlabel('t')
         ax_traj.set_ylabel('x,y')
         for i in range(args.num_bundles):
-            ax_traj.plot(true_y.cpu().numpy()[:, i,0],true_y.cpu().numpy()[:, i,1],
-                         'g-')
-            ax_traj.plot(pred_y.cpu().numpy()[:,i], pred_ydot.cpu().numpy()[:, i])
+            # ax_traj.plot(t.detach().cpu().numpy(), true_y.cpu().numpy()[:, i,0],
+            #              'g-')
+            ax_traj.plot(t.detach().cpu().numpy(), pred_y.cpu().numpy()[:, i])
         ax_phase.set_yscale('log')
         ax_phase.plot(np.arange(len(lst)), lst)
 
-        # ax_traj.legend()
+        ax_traj.legend()
 
         plt.draw()
         plt.pause(0.001)
@@ -257,14 +243,14 @@ if __name__ == '__main__':
     #need to sample tuple of (a1,f,IC)
     # each column of Wouts defines a solution thus, each tuple defines a solution too
 
-    mu = 0
-    f_train = [lambda z: 0.*z, lambda z: 1 + 0*z, lambda z: torch.cos(z), lambda z: torch.sin(z)]
-    a0_train = [lambda z: 1. +0.*z, lambda z: 3*z, lambda z: z**2 ]
-    a1_train = [lambda z: 0*z,lambda z: z**2, lambda z: z**3]
+
+    f_train = [lambda z: 0*z]
+    a0_train = [lambda z: 1.5 + 1.*torch.cos(z), lambda z: 1.5 + 4*torch.cos(z) ]
+    a1_train = [lambda z: 0*z]
     r1 = -5.
     r2 = 5.
-    true_y0 = (r2 - r1) * torch.rand(100,2) + r1
-    # true_y0 = torch.tensor([1.,0.]).reshape(1,2)#[:,1] = 0
+    true_y0 = torch.ones(100,2)#(r2 - r1) * torch.rand(100,2) + r1
+
     # print(true_y0.shape)
     t = torch.arange(0., args.tmax, args.dt).reshape(-1, 1)
     t.requires_grad = True
@@ -297,7 +283,7 @@ if __name__ == '__main__':
 
     loss_collector = []
 
-    best_residual = 1e-1
+    best_residual = 1e-3
 
     if not args.evaluate_only:
 
@@ -308,12 +294,12 @@ if __name__ == '__main__':
             t0 = torch.tensor([[0.]])
             t0.requires_grad = True
 
-            # tmax = torch.tensor([[4*np.pi]])
-            # tmax.requires_grad = True
+            tmax = torch.tensor([[np.pi]])
+            tmax.requires_grad = True
 
-            tr = args.tmax * torch.rand(int(args.tmax / args.dt))[:20].reshape(-1, 1)
-            tr.requires_grad = True
-            tv = torch.cat([t0, tr], 0)
+            trand = args.tmax * torch.rand(int(args.tmax / args.dt)).reshape(-1, 1)
+            trand.requires_grad = True
+            tv = torch.cat([t0, trand,tmax], 0)
             # tv.requires_grad = True
             optimizer.zero_grad()
 
@@ -327,10 +313,10 @@ if __name__ == '__main__':
             loss_diffeq = pred_yddot - get_udot(tv,pred_y,pred_ydot,a1_samples,a0_samples,f_samples)
             # loss_diffeq = (a1(tv.detach()).reshape(-1, 1)) * pred_ydot + (a0(tv.detach()).reshape(-1, 1)) * pred_y - f(
             #     tv.detach()).reshape(-1, 1)
-            # print(pred_y[0,:])
-            # print(pred_y[-1,:])
+            print(pred_y[0,:])
+            print(pred_y[-1,:])
             # enforce initial conditions
-            loss_ics = torch.mean((pred_y[0, :].ravel() - y0_samples[:,0].ravel())**2) + torch.mean((pred_ydot[0,:].ravel()-y0_samples[:,1].ravel())**2)
+            loss_ics = torch.mean((pred_y[0, :].ravel() - y0_samples[:,0].ravel())**2,0) + torch.mean((pred_y[-1,:].ravel()-y0_samples[:,1].ravel())**2,0)
 
             # print(loss_ics)
 
@@ -350,7 +336,7 @@ if __name__ == '__main__':
                 pred_yddot = pred_yddot.detach()
 
                 # pred_y = pred_y.reshape(-1, args.num_bundles)
-                visualize(true_y.detach(), pred_y.detach(),pred_ydot, loss_collector,t.detach())
+                visualize(true_y.detach(), pred_y.detach(), loss_collector,t.detach())
                 ii += 1
 
                 # current_residual = (pred_yddot - get_udot(t, pred_y, pred_ydot, a1_samples, a0_samples, f_samples)) ** 2
@@ -362,7 +348,7 @@ if __name__ == '__main__':
                 # print(current_residual)
                 if current_residual < best_residual:
 
-                    torch.save(func.state_dict(), 'func_ffnn_bundles_vdp')
+                    torch.save(func.state_dict(), 'func_ffnn_bundles_second_order')
                     best_residual = current_residual
                     print(itr,best_residual)
         # torch.save(func.state_dict(), 'func_ffnn_bundles')
@@ -376,31 +362,29 @@ if __name__ == '__main__':
     # r2 = 15.
     # true_y0 = (r2 - r1) * torch.rand(100) + r1
 
-    func.load_state_dict(torch.load('func_ffnn_bundles_vdp'))
+    func.load_state_dict(torch.load('func_ffnn_bundles_second_order'))
     func.eval()
 
-    f_train = [lambda z: 0. * z, lambda z: 1 + 0 * z, lambda z: torch.cos(z), lambda z: torch.sin(z)]
-    a0_train = [lambda z: 1. + 0. * z, lambda z: 3 * z, lambda z: z ** 2]
-    a1_train = [lambda z: 0 * z, lambda z: z ** 2, lambda z: z ** 3]
-    r1 = -5.
-    r2 = 5.
-    true_y0 = (r2 - r1) * torch.rand(100, 2) + r1
-    t = torch.arange(0., args.tmax, args.dt).reshape(-1, 1)
-    t.requires_grad = True
+
+
+    # plt.figure()
+    #
+    # plt.plot(h)
+    # plt.show()
+
+    alphas = torch.linspace(1.,2,50)
+    betas = torch.linspace(2, 5, 50)
+    Alphas,Betas = torch.meshgrid(alphas,betas)
+    new_Alphas = Alphas.ravel()
+    new_Betas = Betas.ravel()
+    true_y0 = torch.tensor([1.,1.]).reshape(1,2)#(r2 - r1) * torch.rand(1, 2) + r1
+    ts = torch.arange(0., args.tmax, args.dt).reshape(-1, 1)
+    ts.requires_grad = True
+
     tmax = torch.tensor([[np.pi]])
     tmax.requires_grad = True
 
-    t = torch.cat([t, tmax], 0)
-
-    # sample each parameter to build the tuples
-    f_samples = random.choices(f_train, k=args.num_bundles)
-    a0_samples = random.choices(a0_train, k=args.num_bundles)
-    a1_samples = random.choices(a1_train, k=args.num_bundles)
-    y0_samples = true_y0[torch.tensor(random.choices(range(len(true_y0)), k=args.num_bundles))]
-
-    diffeq_init = diffeq(a1_samples, a0_samples, f_samples)
-    gt_generator = base_diffeq(diffeq_init)
-    true_y = gt_generator.get_solution(y0_samples, t.ravel())
+    t = torch.cat([ts,tmax],0)
 
     h = func.h(t)
     hd = diff(h, t)
@@ -410,9 +394,92 @@ if __name__ == '__main__':
     hdd = hdd.detach()
 
 
-    wout = get_wout(h, hd, hdd, true_y0, t.detach(), a1_samples, a0_samples, f_samples)
-    pred_y = h@wout
-    pred_yd = hd@wout
-    pred_ydd = hdd @ wout
-    current_residual = torch.mean((pred_ydd - get_udot(t, pred_y, pred_yd, a1_samples, a0_samples, f_samples)) ** 2)
-    print(current_residual)
+    f_train = []
+    a1_train = []
+
+
+    # for i in range(len(new_Betas)):
+    #
+    #     print(new_Alphas[i]+new_Betas[i]*torch.cos(t))
+
+    # print(new_Alphas)
+    # print(new_Betas)
+    # plt.figure()
+    binary_vars = []
+    epsilon = 1e-3
+
+    for i in range(len(new_Betas)):
+        a0_train = [lambda z: 0*z]
+        a1_train = [lambda z: new_Alphas[i].item()+new_Betas[i]*torch.cos(z)]
+        f_train = [lambda z: 0*z]
+        wout = get_wout(h, hd, hdd, true_y0, t.detach(), a1_train, a0_train, f_train)
+        pred_y = h @ wout
+
+        # print(pred_y)
+        if (torch.abs(pred_y[-1,0])-1.)>epsilon or ((torch.abs(pred_y[0,0])-1.)>epsilon):
+            binary_vars.append(0)
+        else:
+            binary_vars.append(1)
+        # plt.plot(pred_y)
+    # plt.yscale('log')
+    # plt.show()
+
+    binary_vars = torch.tensor(binary_vars)
+    plt.figure()
+    plt.contourf(alphas,betas,binary_vars.reshape(-1,alphas.shape[0]))
+    plt.show()
+
+    # for i in range(len(new_Alphas)):
+        # print(new_Alphas[i],new_Betas[i])
+    # a0_train= [lambda t: new_Alphas[i].item() + new_Betas[i].item() * torch.cos(t) for i in range(len(new_Betas))]
+    # f_train.append(lambda t=t: 0 * t)
+    # a1_train.append(lambda t=t: 0 * t)
+
+    # a0_train.append(1+)
+
+    # a0_batch = torch.cat([var_(t) for var_ in a0_train], 1)
+    # a1_batch = torch.cat([var_(t) for var_ in a1s], 1)
+    # f_batch = torch.cat([var_(t) for var_ in fs], 1)
+
+    # print(len(a0_train))
+    # print(a0_train[0](t))
+    # print(a0_train[11](t))
+
+
+        # diffeq_init = diffeq(a1_samples,a0_samples, f_samples)
+        # gt_generator = base_diffeq(diffeq_init)
+
+
+
+
+    # s1 = time.time()
+    # s2 = time.time()
+    # print(f'all_ics:{s2 - s1}')
+    #
+    # s1 = time.time()
+    # true_ys = (gt_generator.get_solution(y0_samples, t.ravel())).reshape(-1, args.num_bundles_test)
+    # s2 = time.time()
+    # print(f'gt_ics:{s2 - s1}')
+    #
+    # print(true_ys.shape,pred_y.shape)
+
+    # s1 = time.time()
+    # true_y = estim_generator.get_solution(ics.reshape(-1, 1), t.ravel())
+    # estim_ys = true_y.reshape(len(pred_y), ics.shape[1])
+    # s2 = time.time()
+    # print(f'estim_ics:{s2 - s1}')
+
+    # print(f'prediction_accuracy:{((pred_y - true_ys) ** 2).mean()} pm {((pred_y - true_ys) ** 2).std()}')
+    # print(f'estim_accuracy:{((estim_ys - true_ys) ** 2).mean()} pm {((estim_ys - true_ys) ** 2).std()}')
+
+    # fig, ax = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
+    # # print(true_ys[0,:])
+    # for i in range(0, len(new_Alphas)):
+    #     # ax[0].plot(t.detach().cpu().numpy(), true_ys.cpu().numpy()[:, i], c='blue', linestyle='dashed')
+    #     ax[0].plot(t.detach().cpu().numpy(), pred_y.cpu().numpy()[:, i], c='orange')
+    #     # plt.draw()
+    # #
+    # # ax[1].plot(t.detach().cpu().numpy(), ((true_ys - pred_y) ** 2).mean(1).cpu().numpy(), c='green')
+    # # ax[1].set_xlabel('Time (s)')
+    # # plt.legend()
+    # plt.show()
